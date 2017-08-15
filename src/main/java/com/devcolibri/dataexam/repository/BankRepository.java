757fba9d3c991d5775040e5d02103657dae9daf7
Kowi77 <kovalenka@mail.ru>
2017-08-15 23:42:33 +0700

    ver 1.0.1

---
+++ src/main/java/com/devcolibri/dataexam/repository/BankRepository.java
@@ -0,0 +1,9 @@
+package com.devcolibri.dataexam.repository;
+
+import com.devcolibri.dataexam.entity.Bank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankRepository extends JpaRepository<Bank, Long> {
+
+}
+
